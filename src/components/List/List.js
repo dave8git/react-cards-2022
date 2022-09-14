b05da7aa234c8d991b2880b4872c622a2980fbc7
unknown <dave8git@gmail.com>
2022-09-14 19:49:37 +0200

    add container for columns

---
+++ src/components/List/List.js
@@ -0,0 +1,19 @@
+import styles from './List.module.scss';
+import Column from '../Column/Column'
+const List = () => {
+    return (
+        <div>
+            <header className={styles.header}>
+                <h2>Things to do<span> soon</span></h2>
+            </header>
+            <p className={styles.description}>Interesting thigns I want to check out.</p>
+            <section className={styles.columns}>
+               <Column title={"Books"} icon={"book"}/>
+               <Column title={"Movies"} icon={"film"}/>
+               <Column title={"Games"} icon={"play"}/>
+            </section>
+        </div>
+    );
+};
+
+export default List;
