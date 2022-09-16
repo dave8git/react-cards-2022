d08ff0071a8a227e4ea637a4def31d15ef637a73
unknown <dave8git@gmail.com>
2022-09-16 17:01:03 +0200

    Add ColumnForm.js

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,6 +1,29 @@
 import styles from './List.module.scss';
-import Column from '../Column/Column'
+import Column from '../Column/Column';
+import ColumnForm from '../ColumnForm/ColumnForm';
+import { useEffect, useState } from 'react';
+import shortid from 'shortid';
+
 const List = () => {
+    const [columns, setColumns] = useState([
+        {id: 1, title: 'Books', icon: 'book'}, 
+        {id: 2, title: 'Movies', icon: 'film'},
+        {id: 3, title: 'Games', icon: 'gamepad'},
+    ]); 
+
+    // useEffect(() => {
+    //     setTimeout(() => {
+    //         setColumns([...columns, {id: 4, title: 'Test column'}]);
+    //         console.log(columns);
+    //     }, 2000);
+    // },[]);
+
+    const addColumn = newColumn => {
+        setColumns([...columns, { id: shortid(), title: newColumn.title }]);
+      };
+    
+      
+  
     return (
         <div>
             <header className={styles.header}>
@@ -8,10 +31,12 @@ const List = () => {
             </header>
             <p className={styles.description}>Interesting thigns I want to check out.</p>
             <section className={styles.columns}>
-               <Column title={"Books"} icon={"book"}/>
+                {columns.map(column => <Column key={column.id} title={column.title} icon={column.icon} />)};
+               {/* <Column title={"Books"} icon={"book"}/>
                <Column title={"Movies"} icon={"film"}/>
-               <Column title={"Games"} icon={"play"}/>
+               <Column title={"Games"} icon={"play"}/> */}
             </section>
+            <ColumnForm action={addColumn} />
         </div>
     );
 };
