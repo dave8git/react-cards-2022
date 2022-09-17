e89f4770d551fabae0efabf57f133e94c7363006
unknown <dave8git@gmail.com>
2022-09-18 00:50:46 +0200

    ADD cards

--- src/components/ColumnForm/ColumnForm.js
+++ src/components/ColumnForm/ColumnForm.js
@@ -1,21 +1,23 @@
 import styles from './ColumnForm.module.scss';
-
+import Button from '../Button/Button';
 import { useEffect, useState } from 'react';
 
 const ColumnForm = props => {
-    const [value, setValue] = useState('');
+    const [title, setValue] = useState('');
+    const [icon, setIcon] = useState('');
 
     const handleSubmit = e => {
         e.preventDefault();
-        props.action({ title: value });
+        props.action({ title: title, icon: icon });
         setValue('');
     }
     
 
     return (
-        <form onSubmit={handleSubmit}>
-            <input type="text" value={value} onChange={e => setValue(e.target.value)} />
-            <button>Add column</button>
+        <form className={styles.columnForm} onSubmit={handleSubmit}>
+            Title: <input type="text" value={title} onChange={e => setValue(e.target.value)} />
+            Icon: <input type="text" value={icon} onChange={e => setIcon(e.target.value)} />
+            <Button>Add column</Button>
         </form>
     )
 }
