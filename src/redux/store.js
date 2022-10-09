82a0031ad6b2347567b25ff31dbfe563e1ae6024
unknown <dave8git@gmail.com>
2022-10-09 17:24:56 +0200

    add filtering cards working

--- src/redux/store.js
+++ src/redux/store.js
@@ -2,15 +2,19 @@ import { createStore } from 'redux';
 import initialState from './initialState';
 
 const reducer = (state, action) => {
+    console.log(action);
     switch(action.type) {
         case 'ADD_COLUMN': 
             return {...state, columns: [...state.columns, action.newColumn]};
         case 'ADD_CARD': 
             return { ...state, cards: [...state.cards, action.newCard]};
+        case 'UPDATE_SEARCHSTRING':
+            return {...state, searchString: action.newSearch }
         default: 
             return state;
     }
 };
+
 
 const store = createStore(
     reducer, 
