e700b2facb55af4e330c5ffe657a497bc1abd3c0
dave8git <dave8git@gmail.com>
2022-10-13 21:40:38 +0200

    lists added to initialState.js

--- src/redux/initialState.js
+++ src/redux/initialState.js
@@ -27,7 +27,19 @@ const initialState = {
     ],
 
     searchString: '',
-  
+    
+    lists: [
+      {
+        id: 1,
+        title: 'Things to do...', 
+        description: 'Interesting things I want to check out'
+      },
+      {
+        id: 2,
+        title: 'Test list',
+        description: 'Lorem Ipsum'
+      }
+    ]
   };
   
   export default initialState;
