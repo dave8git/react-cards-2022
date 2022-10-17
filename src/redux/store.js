5ddee672fd7ad7b43bb31b7fff03c2f9db6a7840
dave8git <dave8git@gmail.com>
2022-10-18 00:01:51 +0200

    removeCard works

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,35 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import initialState from './initialState';
-import { stringsEqual } from '../utils/strContains';
 import listsReducer from './listsRedux';
 import columnsReducer from './columnsRedux';
 import cardsReducer from './cardsRedux';
 import searchStringReducer from './searchStringReducer';
 import shortid from 'shortid';
 
-// selectors 
-export const getFilteredCards = ({ cards, searchString }, columnId) => cards.filter(card => card.columnId === columnId && stringsEqual(card.title, searchString));
 
-export const getAllColumns = state => state.columns;
-
-export const getListById = ({ lists }, listId) => lists.find(list => list.id === listId);
-
-export const getColumnsByList = ({columns}, listId) => columns.filter(column => column.listId === listId);
-
-export const getAllLists = state => state.lists;
-
-export const getFavoriteCard = (state) => state.cards.filter(card => card.isFavorite === true);
-
-// action creators 
-export const addColumn = payload => ({type: 'ADD_COLUMN', payload});
-
-export const addCard = payload => ({type: 'ADD_CARD', payload});
-
-export const addList = payload => ({type: 'ADD_LIST', payload});
-
-export const favoriteCard = payload => ({type: 'FAVORITE_CARD', payload});
-
-export const updateSearchString = payload => ({type: 'UPDATE_SEARCHSTRING', payload});
 
 // const reducer = (state, action) => {
 //     console.log(action);
